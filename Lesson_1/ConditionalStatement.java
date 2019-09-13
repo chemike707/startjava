c167cd09fae87461a960bf2c81a27c8a5858626f
Mihail <chemike707.gmail.com>
2019-09-13 22:03:07 +0800

    bug fix2

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,31 +1,29 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-        int yourAge = 21;        
-        if(yourAge > 20) {
+        int age = 21;        
+        if(age > 20) {
             System.out.println("Вы старше 20");
-         }  
+        }  
         
-        boolean youMale = true;            
-        if(youMale) {
+        boolean male = false;            
+        if(male) {
              System.out.println("Вы мужчина");
-        } 
-
-        boolean youWoman = true;            
-        if(youWoman) {
+        }                    
+        if(!male) {
              System.out.println("Вы женщина");
         } 
         
-        float yourHeight = 1.70F;
-        if(yourHeight < 1.80) {
+        float height = 1.70F;
+        if(height < 1.80) {
             System.out.println("Коротышка");
         } else{
             System.out.println("Высокий");
         }
         
-        char nameWord = 'M';
-        if(nameWord == 'M') {
+        char capitalinName = 'M';
+        if(capitalName == 'M') {
             System.out.println("Ты наверное Михаил");
-        } else if(nameWord == 'I') {
+        } else if(capitalName == 'I') {
             System.out.println("Ты наверное Iван");
         } else{
             System.out.println("Мы не знаем кто ты");
