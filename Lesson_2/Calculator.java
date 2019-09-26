0ab5398d434022281797a5a87c21f47ee73a0bd8
Mihail <chemike707.gmail.com>
2019-09-26 17:20:50 +0800

    bug fix3

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -48,10 +48,9 @@ public class Calculator {
             case '^':   
                 int result = 1;
                 for(int i = 1; i <= number2; i++) {
-                result *= number1;
+                    result *= number1;
                 }
                 System.out.println(result);
-                break;
         }
     }
 }
