2a8be414c6f706999d0530d2519e08bf8b69fc2e
Mihail <chemike707.gmail.com>
2019-10-17 23:28:16 +0800

    implement hw4 bag fix1

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -1,9 +1,18 @@
 package com.startjava.lesson_4.calculator;
 
 public class Calculator {
+    private String mathExpression;
     private int numberOne;
     private int numberTwo;
     private char sign;
+
+    public  Calculator(String mathExpression) {
+        this.mathExpression = mathExpression;
+    }
+
+    public String getMathExpression() {
+        return mathExpression;
+    }
 
     public void setNumberOne(int numberOne) {
         this.numberOne = numberOne;
@@ -16,7 +25,6 @@ public class Calculator {
     public void setSign(char sign) {
         this.sign = sign;
     }
-
 
     public void calculate() {
         switch(sign) {
@@ -37,7 +45,9 @@ public class Calculator {
                 break;
             case '^':
                 System.out.println(Math.pow(numberOne, numberTwo));
-
+                break;
+            default:
+                System.out.println("error");
         }
     }
 }
