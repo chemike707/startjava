cfde9891d4b83e1d26a7f9bfc7480b61d3896e01
Mihail <chemike707.gmail.com>
2019-10-17 23:28:27 +0800

    implement hw4 bag fix1

--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -9,21 +9,19 @@ public class CalculatorTest {
             Scanner scan = new Scanner(System.in);
             System.out.print("Введите математическое выражение: ");
 
-            String string = scan.nextLine();
-            String[] mathOperation = string.split(" ");
+            Calculator calculator = new Calculator(scan.nextLine());
+            String[] mathExpression = calculator.getMathExpression().split(" ");
 
-            Calculator calculator = new Calculator();
-
-            calculator.setNumberOne(Integer.parseInt(mathOperation[0]));
-            calculator.setSign(mathOperation[1].charAt(0));
-            calculator.setNumberTwo(Integer.parseInt(mathOperation[2]));
+            calculator.setNumberOne(Integer.parseInt(mathExpression[0]));
+            calculator.setSign(mathExpression[1].charAt(0));
+            calculator.setNumberTwo(Integer.parseInt(mathExpression[2]));
 
             calculator.calculate();
 
             do {
                 System.out.println("Хотите продолжить? [да/нет]: ");
                 escCalculator = scan.next();
-            } while(!escCalculator.equals("yes") & !escCalculator.equals("no"));
+            } while(!escCalculator.equals("yes") && !escCalculator.equals("no"));
         } while (escCalculator.equals("yes"));
     }
 }
